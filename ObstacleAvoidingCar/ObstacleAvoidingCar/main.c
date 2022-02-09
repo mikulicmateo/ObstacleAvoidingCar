ce21fd2c83635f9fd1e7e0061c89d04e90dfc68e
Mateo Mikulic <mikulic.dev@gmail.com>
2022-02-09 14:41:43 +0100

    Project created

---
+++ ObstacleAvoidingCar/ObstacleAvoidingCar/main.c
@@ -0,0 +1,18 @@
+/*
+ * ObstacleAvoidingCar.c
+ *
+ * Created: 9.2.2022. 14:38:37
+ * Authors : Mateo Mikulic, Dominik Vicevic, Mauro Gizdulic
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
