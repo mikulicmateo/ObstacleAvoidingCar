2bca14e905a963c01e9523c4e579aaf4ac6a3717
Mateo Mikulic <mikulic.dev@gmail.com>
2022-02-10 18:01:04 +0100

    Test LCD

--- ObstacleAvoidingCar/ObstacleAvoidingCar/main.c
+++ ObstacleAvoidingCar/ObstacleAvoidingCar/main.c
@@ -6,13 +6,20 @@
  */ 
 
 #include <avr/io.h>
-
+#include "lcd.h"
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRD = _BV(4);
+
+	TCCR1A = _BV(COM1B1) | _BV(WGM10);
+	TCCR1B = _BV(WGM12) | _BV(CS11);
+	OCR1B = 128;
+
+	lcd_init(LCD_DISP_ON);
+	lcd_clrscr();
+	lcd_puts("Hello World");
+
+	while (1);
 }
 
