be33ae05ed77210b598dede1e6ce554fc3688aa7
Mateo Mikulic <mikulic.dev@gmail.com>
2022-02-10 20:48:02 +0100

    US sensor without input capture

--- ObstacleAvoidingCar/ObstacleAvoidingCar/main.c
+++ ObstacleAvoidingCar/ObstacleAvoidingCar/main.c
@@ -13,9 +13,40 @@
 #include <stdlib.h>
 #include "lcd.h"
 
-#define Trigger_pin PA0
+#define TRIGGER_PIN PD7
 
 volatile int TimerOverflow = 0;
+
+void calculateDistance(uint8_t echo_pin){
+	char string[10];
+	PORTD |= (1 << TRIGGER_PIN);
+	_delay_us(10);
+	PORTD &= (~(1 << TRIGGER_PIN));
+	
+	TCNT1 = 0;	/* Clear Timer counter */
+	TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
+
+	while((PIND & _BV(echo_pin)) == 0); // while 0, wait for high
+	
+	//reset timer and overflow
+	TCNT1 = 0;	/* Clear Timer counter */
+	TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
+	TimerOverflow = 0;/* Clear Timer overflow count */
+	
+	
+	while(PIND & _BV(echo_pin)); // while 1, wait for low
+	long count = TCNT1 + (65535 * TimerOverflow); //calculate how many ticks the echo pin was HIGH
+	double distance = (double)count/427.21; // calculate distance
+	
+	dtostrf(distance, 2, 2, string);/* distance to string */
+	strcat(string, " cm   ");	/* Concat unit i.e.cm */
+	lcd_gotoxy(2, 0);
+	lcd_puts("Dist = ");
+	lcd_gotoxy(2, 7);	/* Print distance */
+	lcd_puts(string);
+}
+
+
 
 ISR(TIMER1_OVF_vect)
 {
@@ -28,8 +59,8 @@ int main(void)
 	long count;
 	double distance;
 	
-	DDRA = 0x01;		/* Make trigger pin as output */
-	PORTD = 0xFF;		/* Turn on Pull-up */
+	DDRD = _BV(TRIGGER_PIN);		/* Make trigger pin as output */
+	PORTD = _BV(PIND0);		/* Turn on Pull-up */
 	
 	///////////lcd///////////////
 	DDRB = _BV(PB3);
@@ -47,40 +78,12 @@ int main(void)
 	sei();			/* Enable global interrupt */
 	TIMSK = (1 << TOIE1);	/* Enable Timer1 overflow interrupts */
 	TCCR1A = 0;		/* Set all bit to zero Normal operation */
+	TCCR1B = _BV(CS10);
 
 	while(1)
 	{
-		/* Give 10us trigger pulse on trig. pin to HC-SR04 */
-		PORTA |= (1 << Trigger_pin);
-		_delay_us(10);
-		PORTA &= (~(1 << Trigger_pin));
-		
-		TCNT1 = 0;	/* Clear Timer counter */
-		TCCR1B = 0x41;	/* Capture on rising edge, No prescaler*/
-		TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
-		TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
-
-		/*Calculate width of Echo by Input Capture (ICP) */
-		
-		while ((TIFR & (1 << ICF1)) == 0);/* Wait for rising edge */
-		TCNT1 = 0;	/* Clear Timer counter */
-		TCCR1B = 0x01;	/* Capture on falling edge, No prescaler */
-		TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
-		TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
-		TimerOverflow = 0;/* Clear Timer overflow count */
-
-		while ((TIFR & (1 << ICF1)) == 0);/* Wait for falling edge */
-		count = ICR1 + (65535 * TimerOverflow);	/* Take count */
-		/* 8MHz Timer freq, sound speed =343 m/s */
-		distance = (double)count / 466.47;
-
-		dtostrf(distance, 2, 2, string);/* distance to string */
-		strcat(string, " cm   ");	/* Concat unit i.e.cm */
-		lcd_gotoxy(2, 0);
-		lcd_puts("Dist = ");
-		lcd_gotoxy(2, 7);	/* Print distance */
-		lcd_puts(string);
-		_delay_ms(200);
+		calculateDistance(PIND0);	
+		_delay_ms(500);
 	}
 }
 
