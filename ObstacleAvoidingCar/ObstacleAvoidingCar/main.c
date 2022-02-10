9e52fb73a29cd82eafc155d5f63478e02e1d066b
Mateo Mikulic <mikulic.dev@gmail.com>
2022-02-10 19:10:54 +0100

    HCSR04 test

--- ObstacleAvoidingCar/ObstacleAvoidingCar/main.c
+++ ObstacleAvoidingCar/ObstacleAvoidingCar/main.c
@@ -5,21 +5,82 @@
  * Authors : Mateo Mikulic, Dominik Vicevic, Mauro Gizdulic
  */ 
 
+#define F_CPU 7372800UL
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdlib.h>
 #include "lcd.h"
+
+#define Trigger_pin PA0
+
+volatile int TimerOverflow = 0;
+
+ISR(TIMER1_OVF_vect)
+{
+	TimerOverflow++;	/* Increment Timer Overflow count */
+}
 
 int main(void)
 {
-	DDRD = _BV(4);
+	char string[10];
+	long count;
+	double distance;
+	
+	DDRA = 0x01;		/* Make trigger pin as output */
+	PORTD = 0xFF;		/* Turn on Pull-up */
+	
+	///////////lcd///////////////
+	DDRB = _BV(PB3);
 
-	TCCR1A = _BV(COM1B1) | _BV(WGM10);
+	/*TCCR1A = _BV(COM1B1) | _BV(WGM10);
 	TCCR1B = _BV(WGM12) | _BV(CS11);
-	OCR1B = 128;
+	OCR1B = 128;*/
+	TCCR0 =  _BV(WGM01) | _BV(WGM00) | _BV(CS01) | _BV(COM01);
+	OCR0 = 128;
 
 	lcd_init(LCD_DISP_ON);
 	lcd_clrscr();
-	lcd_puts("Hello World");
+	/////////////////////////////
+	
+	sei();			/* Enable global interrupt */
+	TIMSK = (1 << TOIE1);	/* Enable Timer1 overflow interrupts */
+	TCCR1A = 0;		/* Set all bit to zero Normal operation */
 
-	while (1);
+	while(1)
+	{
+		/* Give 10us trigger pulse on trig. pin to HC-SR04 */
+		PORTA |= (1 << Trigger_pin);
+		_delay_us(10);
+		PORTA &= (~(1 << Trigger_pin));
+		
+		TCNT1 = 0;	/* Clear Timer counter */
+		TCCR1B = 0x41;	/* Capture on rising edge, No prescaler*/
+		TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
+		TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
+
+		/*Calculate width of Echo by Input Capture (ICP) */
+		
+		while ((TIFR & (1 << ICF1)) == 0);/* Wait for rising edge */
+		TCNT1 = 0;	/* Clear Timer counter */
+		TCCR1B = 0x01;	/* Capture on falling edge, No prescaler */
+		TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
+		TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
+		TimerOverflow = 0;/* Clear Timer overflow count */
+
+		while ((TIFR & (1 << ICF1)) == 0);/* Wait for falling edge */
+		count = ICR1 + (65535 * TimerOverflow);	/* Take count */
+		/* 8MHz Timer freq, sound speed =343 m/s */
+		distance = (double)count / 466.47;
+
+		dtostrf(distance, 2, 2, string);/* distance to string */
+		strcat(string, " cm   ");	/* Concat unit i.e.cm */
+		lcd_gotoxy(2, 0);
+		lcd_puts("Dist = ");
+		lcd_gotoxy(2, 7);	/* Print distance */
+		lcd_puts(string);
+		_delay_ms(200);
+	}
 }
 
